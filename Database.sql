dea0278623aa5bd485cf0358717a3247e14e938f
Elias <eliasmwita86@gmail.com>
2024-03-18 07:22:48 +0300

    It is me again

---
+++ Database.sql
@@ -0,0 +1,9 @@
+CREATE TABLE customer_details (
+    id int primary key,
+    first_name varchar(50),
+    last_name varchar(50),
+    email varchar(20),
+    phone_Number int,
+);
+SHOW TABLES;
+DESCRIBE customer_details;
